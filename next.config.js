b2660df488ed12ee088f1755b6331085d64bbeeb
ameeshagireesh <ameesha.gireeshkm@gmail.com>
2023-03-04 22:07:35 +0530

    feat: refreshToken ccheck middleware

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/login',
+        has: [
+          {
+            type: 'cookie',
+            key: 'refreshToken',
+          },
+        ],
+        permanent: false,
+        destination: '/',
+      },
+      {
+        source: '/',
+        missing:[
+          {type:'cookie',key:'refreshToken'}
+        ],
+        permanent:false,
+        destination:'/login'
+      }
+    ]
+  }
 }
-
 module.exports = nextConfig
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,10 @@
-import Onboarding from '../components/Onboarding/onboarding';
 
-
-export default function Home({authUrl}) {
+export default function Home() {
   return (
     <>
-    <Onboarding authUrl={authUrl} />
-    {/* If cookie is present then show dashboard if not present then show OnBoarding screen */}
+      <div>
+        Dashboard
+      </div>
     </>
   )
-}
-
-// serversideprops to get cookie and check
-export async function getServerSideProps(context) {
-  // Check cookie if cookie is present then dont do the next steps
-  const response = await fetch('http://localhost:3000/api/auth/google');
-  const { url } = await response.json();
-  return {
-    props: { authUrl: url }, // will be passed to the page component as props
-  }
-  // if cookie 
 }
