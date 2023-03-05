88b352ae986f8c18878b329ea3358a6ef78653e8
ameeshagireesh <ameesha.gireeshkm@gmail.com>
2023-03-05 12:46:23 +0530

    something idek bro

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,29 +3,31 @@ import Image from "next/image"
 export default function Login({ authUrl }) {
     return (
         <>
-            <div class="flex flex-col pt-20 p-8 mx-auto my-auto">
-                <h1 class="text-3xl font-bold font-serif">
-                    Hello, let's get started!
-                </h1>
-                <p class="text-gray-400">
-                    Register for the event here and don't worry
-                </p>
+            <div class="flex flex-col pt-20 p-10 mx-auto my-auto">
+                <div class="pb-10">
+                    <h1 class="text-xl font-bold font-serif ">
+                        Hello, let's get started!
+                    </h1>
+                    <p class="text-gray-400 text-sm">
+                        Register for the event here and don't worry
+                    </p>
+                </div>
                 <Image class="mx-auto my-autopt-3 pb-3"
                     src="/voucher.svg"
                     width={200}
                     height={200}>
                 </Image>
-                <h3 class="text-3xl font-bold font-serif">
-                    Register for Sunburn'23
-                </h3>
-                <p class="text-gray-400">
-                    Sunburn Festival is a commercial electronic dance music festival held in India.
-                </p>
-                <div class="flex pt-5 mx-auto my-auto">
-                    <a href={authUrl} class="bg-zinc-800 hover:bg-zinc-900 rounded border-stone-500 border-2 p-5 flex space-x-3">
-                        <p class="">
-                            Sign in
-                        </p>
+                <div class="pt-16 pb-10">
+                    <h3 class="text-xl font-bold font-serif">
+                        Register for Sunburn'23
+                    </h3>
+                    <p class="text-gray-400 text-sm">
+                        Sunburn Festival is a commercial electronic dance music festival held in India.
+                    </p>
+                </div>
+                <div class="flex justify-center my-auto">
+                    <a href={authUrl} class="bg-zinc-800 hover:bg-zinc-900 rounded border-stone-500 border-2 flex space-x-3 w-full h-14 items-center justify-center">
+                            Sign in!
                     </a>
                 </div>
             </div>
@@ -33,10 +35,10 @@ export default function Login({ authUrl }) {
     )
 }
 
-export async function getServerSideProps(context) {  
-    const response = await fetch('http://localhost:3000/');
+export async function getServerSideProps(context) {
+    const response = await fetch('http://localhost:3000/api/auth/google');
     const { url } = await response.json();
     return {
-      props: { authUrl: url }, // will be passed to the page component as props
+        props: { authUrl: url }, // will be passed to the page component as props
     }
-  }
+}
